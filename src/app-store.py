d9646013844e37063e796127a28c9b8e5ea3074f
asalazar516 <asalazar51694@gmail.com>
2020-02-17 23:14:00 -0800

    Added monitor check function

--- src/app-store.py
+++ src/app-store.py
@@ -2,6 +2,29 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
+def monitor_site(version, url, headers):
+    check = True
+
+    while check is True:
+        response = requests.get (url, headers)
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        for item in soup.select("p.whats-new__latest__version"):
+            new_version = item.get_text()
+
+        if (new_version == version):
+            print(f"{new_version}")
+            time.sleep(45)
+            continue
+        else:
+            t = time.localtime
+            current_time = time.strftime("%H:%M:%S", t)
+            current_date = time.strftime("%B %d, %Y", t)
+            print(f"Version has changed from {version} to {new_version}!")
+            print(f"Updated at {current_time} at {current_date}")
+            check = False
+
 
 def main():
     url = "https://apps.apple.com/nz/app/marvel-strike-force-squad-rpg/id1292952049"
@@ -14,13 +37,8 @@ def main():
 
     for item in soup.select("p.whats-new__latest__version"):
         version = item.get_text()
-    
-    print(version)
 
-    if("Version 3.9.0" == version):
-        print("same")
-    else:
-        print("Different")
+    monitor_site(version, url, headers)
 
 if __name__ == "__main__":
     main()
